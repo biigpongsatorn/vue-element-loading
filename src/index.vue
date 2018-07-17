be3816b20c0de7f747885a4b098fa8fa9975c92c
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-07-17 10:32:07 +0700

    ✨ Clean component

--- src/index.vue
+++ src/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
-    <h1>Hello World 🌍 </h1><br>
-    <span> Welcome to vue-element-loading 👏🏻 </span><br>
-    <span>📝 Vue loading component</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'vue-element-loading',
+  name: 'vue-element-loading'
 }
 </script>
 
