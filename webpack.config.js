bc2870c877aa5fb4124fb33c3791deab27a7deab
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-12-08 18:40:15 +0700

    fix: destructure VueLoaderPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './example/main.js',
